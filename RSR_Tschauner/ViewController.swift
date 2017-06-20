8eb262cab00c7dfc71510acf8f871590485cf647
tschauner <munichboi@gmail.com>
2017-06-20 12:47:39 +0200

    Initial Commit

---
+++ RSR_Tschauner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSR_Tschauner
+//
+//  Created by Philipp Tschauner on 20.06.17.
+//  Copyright © 2017 Philipp Tschauner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
